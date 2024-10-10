d651ad9ec173e358a2242e2af50504a4d8b85de4
interniums <intuition228@gmail.com>
2024-10-10 22:02:44 +0300

    added default route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
 // @ts-nocheck
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthProvider'
+import { useEffect } from 'react'
 
 function App() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/socket/home')
+  }, [])
+
   return <Outlet />
 }
 
