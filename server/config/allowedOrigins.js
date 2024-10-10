4756c3c22277526a95eefe91bc766e43a85f44f6
interniums <intuition228@gmail.com>
2024-10-10 23:36:52 +0300

    added cors options

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ['*']
+const allowedOrigins = [
+  'https://chessfull-production.up.railway.app/',
+  'https://chessfull-production-8a3f.up.railway.app/',
+  'https://chessfull-interniums-projects.vercel.app/',
+]
 
 module.exports = allowedOrigins
