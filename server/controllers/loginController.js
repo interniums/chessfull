51f2c7e4e3d88c5986401952f87400b6ebf96c4c
interniums <intuition228@gmail.com>
2024-10-11 14:03:10 +0300

    added error handler for login controller

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -3,49 +3,93 @@ const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+// const handleLogin = asyncHandler(async (req, res, next) => {
+//   const { email, password } = req.body
+//   if (!email || !password) {
+//     return res.status(400).json({ message: 'Invalid data' })
+//   }
+
+//   const user = await User.findOne({ email }).exec()
+//   if (!user) {
+//     return res.status(404).json({ message: 'Invalid email' })
+//   }
+
+//   const match = await bcrypt.compare(password, user.password)
+//   if (!match) {
+//     return res.status(400).json({ message: 'Invalid password' })
+//   }
+
+//   const accessToken = jwt.sign(
+//     {
+//       userInfo: {
+//         username: user.username,
+//         id: user._id,
+//       },
+//     },
+//     process.env.JWT_ACCESS_SECRET,
+//     { expiresIn: '59s' }
+//   )
+
+//   const refreshToken = jwt.sign({ username: user.username, id: user._id }, process.env.JWT_REFRESH_SECRET, {
+//     expiresIn: '7d',
+//   })
+
+//   user.refreshToken = refreshToken
+//   const result = await user.save()
+
+//   res.clearCookie('jwt')
+//   res.cookie('jwt', refreshToken, {
+//     sameSite: 'None',
+//     httpOnly: true,
+//     secure: true,
+//     maxAge: 24 * 60 * 60 * 1000,
+//   })
+
+//   res.json({ accessToken, username: result.username, id: result._id })
+// })
+
 const handleLogin = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body
-  if (!email || !password) {
-    return res.status(400).json({ message: 'Invalid data' })
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Invalid data' })
+    }
+
+    const user = await User.findOne({ email }).exec()
+    if (!user) {
+      return res.status(404).json({ message: 'Invalid email' })
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      return res.status(400).json({ message: 'Invalid password' })
+    }
+
+    const accessToken = jwt.sign(
+      { userInfo: { username: user.username, id: user._id } },
+      process.env.JWT_ACCESS_SECRET,
+      { expiresIn: '59s' }
+    )
+
+    const refreshToken = jwt.sign({ username: user.username, id: user._id }, process.env.JWT_REFRESH_SECRET, {
+      expiresIn: '7d',
+    })
+
+    user.refreshToken = refreshToken
+    await user.save()
+
+    res.cookie('jwt', refreshToken, {
+      sameSite: 'None',
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    })
+
+    res.json({ accessToken, username: user.username, id: user._id })
+  } catch (err) {
+    console.error('Error during login: ', err)
+    res.status(500).json({ message: 'Internal Server Error' })
   }
-
-  const user = await User.findOne({ email }).exec()
-  if (!user) {
-    return res.status(404).json({ message: 'Invalid email' })
-  }
-
-  const match = await bcrypt.compare(password, user.password)
-  if (!match) {
-    return res.status(400).json({ message: 'Invalid password' })
-  }
-
-  const accessToken = jwt.sign(
-    {
-      userInfo: {
-        username: user.username,
-        id: user._id,
-      },
-    },
-    process.env.JWT_ACCESS_SECRET,
-    { expiresIn: '59s' }
-  )
-
-  const refreshToken = jwt.sign({ username: user.username, id: user._id }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: '7d',
-  })
-
-  user.refreshToken = refreshToken
-  const result = await user.save()
-
-  res.clearCookie('jwt')
-  res.cookie('jwt', refreshToken, {
-    sameSite: 'None',
-    httpOnly: true,
-    secure: true,
-    maxAge: 24 * 60 * 60 * 1000,
-  })
-
-  res.json({ accessToken, username: result.username, id: result._id })
 })
 
 module.exports = { handleLogin }
