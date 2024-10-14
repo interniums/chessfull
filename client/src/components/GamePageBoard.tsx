3ad57d0720f85e62e5dd70988ba252349162972c
interniums <intuition228@gmail.com>
2024-10-14 13:30:53 +0300

    changed style for board

--- client/src/components/GamePageBoard.tsx
+++ client/src/components/GamePageBoard.tsx
@@ -762,7 +762,7 @@ export default function GamePageBoard({ mode, players, moves, setMoves, roomId, 
         )}
         <div
           className={
-            !isSmallScreen ? 'xl:h-full xl:min-h-screen' : 'xl:h-full xl:min-h-screen w-full justify-center h-100%'
+            !isSmallScreen ? 'xl:h-full xl:min-h-screen' : 'xl:h-full xl:min-h-screen w-full justify-center h-full'
           }
           style={{ maxWidth: !isSmallScreen ? '85vh' : '', width: !isSmallScreen ? '85vh' : '100%' }}
         >
@@ -796,7 +796,7 @@ export default function GamePageBoard({ mode, players, moves, setMoves, roomId, 
             style={{
               maxWidth: !isSmallScreen ? '85vh' : '',
               width: !isSmallScreen ? '85vh' : '100%',
-              maxHeight: '80vh',
+              maxHeight: '70vh',
               boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 10px',
             }}
           >
